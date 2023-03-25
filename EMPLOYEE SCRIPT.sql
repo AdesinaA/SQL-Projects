f41a5c01810eb7f2f65ad8d5c3b6f2dda94d2e55
Adesina Abdulrahman <53907121+AdesinaA@users.noreply.github.com>
2023-03-25 15:34:22 +0100

    Update EMPLOYEE SCRIPT.sql

--- EMPLOYEE SCRIPT.sql
+++ EMPLOYEE SCRIPT.sql
@@ -1,4 +1,4 @@
--- EXERCISE ONE
+-- TASK 1
 create database if not exists sales;
 use sales;
 CREATE TABLE customers (
@@ -9,14 +9,14 @@ CREATE TABLE customers (
     number_of_complaints INTEGER
 );
 select * from customers;
--- EXERCISE TWO
+-- TASK 2
 CREATE TABLE items (
     item_code VARCHAR(255),
     item VARCHAR(255),
     unit_price NUMERIC(10 , 2 ),
     company_id VARCHAR(255)
 );
--- EXERCISE THREE
+-- TASK 3
 CREATE TABLE companies (
     company_id VARCHAR(255),
     company_name VARCHAR(255),
@@ -25,7 +25,7 @@ CREATE TABLE companies (
 drop table customers;
 drop table items;
 drop table companies;
--- EXERCISE FIVE 
+-- TASK 4 
 CREATE TABLE customers (
     customer_id INTEGER AUTO_INCREMENT,
     first_name VARCHAR(255),
@@ -39,55 +39,55 @@ alter table customers add column gender enum ('M','F') after last_name;
 select * from customers;
 insert into customers (first_name, last_name, gender, email_address, number_of_complaints) value ('John', 'Mackinley', 'M', 'john.mckinley@365careers.com', 0); 
 select * from customers;
--- EXERCISE SIX
+-- TASK 5
 create table companies (company_id varchar (255), company_name varchar (255) default ('X'), headquarter_phone_number varchar (255), unique key (headquarter_phone_number));
--- EXERCISE SEVEN
+-- TASK 6
 select * from companies;
 alter table companies change column headquarter_phone_number headquarter_phone_number varchar(255) not null;
--- EXERCISE EIGHT 
+-- TASK 7
 USE EMPLOYEES;
--- EXERCISE NINE
+-- TASK 8
 SELECT 
     *
 FROM
     EMPLOYEES;
--- EXERCISE TEN
+-- TASK 9
 SELECT 
     *
 FROM
     departments;
--- EXERCISE ELEVEN
+-- TASK 10
 SELECT 
     dept_no
 FROM
     departments;
--- EXERCISE TWELVE    
+-- TASK 11   
 SELECT 
     *
 FROM
     employees;
-    -- EXERCISE 13
+    -- TASK 12
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name = 'Elvis';
--- EXERCISE 14
+-- TASK 13
 SELECT
     *
 FROM
     employees
 WHERE
     gender = 'F' AND first_name = 'Kellie';
--- EXERCISE 15
+-- TASK 14
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name = 'kellie' OR 'Aruna';
--- EXERCISE 16
+-- TASK 15
 SELECT 
     *
 FROM
@@ -95,14 +95,14 @@ FROM
 WHERE
     gender = 'F' AND (first_name = 'kellie'
         OR first_name = 'Bezalel');
--- EXERCISE 17
+-- TASK 16
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name IN ('Dennis' , 'Elvis');
--- EXERCISE 18
+-- TASK 17
 SELECT 
     *
 FROM
@@ -110,56 +110,56 @@ FROM
 WHERE
     gender = 'F'
         AND first_name IN ('Kellie' , 'Bezalel');
--- EXERCISE 19
+-- TASK 18
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name NOT IN ('Mark' , 'John', 'Jacob');
--- EXERCISE 20
+-- TASK 19
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name LIKE ('Mar%');
--- EXERCISE 21
+-- TASK 20
 SELECT 
     *
 FROM
     employees
 WHERE
     hire_date LIKE ('%2000%');
--- EXERCISE 22
+-- TASK 21
 SELECT 
     *
 FROM
     employees
 WHERE
     emp_no LIKE ('1000%');
--- EXERCISE 23
+-- TASK 22
 SELECT 
     *
 FROM
     employees
 WHERE
     first_name LIKE ('Jack%');
--- EXERCISE 24
+-- TASK 23
 SELECT 
     *
 FROM
     EMPLOYEES
 WHERE
     first_name NOT LIKE ('Jack%');
--- EXERCISE 25
+-- TASK 24
 select * from salaries;
 select * from salaries where salary between 60000 and 70000;
--- EXERCISE 26
+-- TASK 25
 SELECT * FROM departments;
 select * from departments where dept_no is null;
 select * from departments where dept_no is not null;
--- EXERCISE 26
+-- TASK 26
 SELECT * FROM salaries;
 SELECT 
     *
@@ -174,7 +174,7 @@ FROM
 WHERE
     gender = 'F'
         AND hire_date > '2000-01-01';
--- EXERCISE 27
+-- TASK 27
 SELECT DISTINCT
     hire_date
 FROM
@@ -183,10 +183,10 @@ SELECT DISTINCT
     first_name, last_name
 FROM
     employees;
--- EXERCISE 28
+-- TASK 28
 SELECT * FROM EMPLOYEES ORDER BY hire_date desc;
 select * from employees order by hire_date asc;
--- EXERCISE 29
+-- TASK 29
 select * from salaries;
 SELECT 
     COUNT(*)
@@ -206,38 +206,38 @@ SELECT DISTINCT
     emp_no
 FROM
     dept_manager;
--- EXERCISE 30
+-- TASK 30
 SELECT * FROM SALARIES;
 SELECT salary, count(emp_no) as 'emps_with_same_salary' from salaries where salary > 80000 group by salary order by salary asc;
--- EXERCISE 31
+-- TASK 31
 SELECT * FROM dept_manager;
 SELECT DISTINCT emp_no, dept_no from dept_manager;
--- EXERCISE 32
+-- TASK 32
 SELECT * FROM SALARIES;
 SELECT * FROM EMPLOYEES;
 SELECT *, AVG(salarY) FROM SALARIES WHERE SALARY > 120000 GROUP BY emp_no order by emp_no;
 SELECT *, AVG(salarY) FROM SALARIES GROUP BY emp_no HAVING salary > 120000;
 select * from employees;
--- EXERCISE 33
+-- TASK 33
 select first_name, count(first_name) as name_count from employees group by first_name having count(first_name) > 253;
--- EXERCISE 34
+-- TASK 34
 select first_name, count(first_name) from employees where hire_date > '1999-01-01' group by first_name having count(first_name) < 200 order by first_name asc;
--- EXERCISE 35
+-- TASK 35
 select * from dept_emp;
 select emp_no, count(emp_no) as count_emp from dept_emp where from_date > '2000-01-01' group by emp_no having count(emp_no) > 1 order by emp_no asc;
--- EXERCISE 36
+-- TASK 36
 select * from employees where emp_no = 99999;
 select * from dept_emp limit 10;
 -- INSERT STATEMENT 
 insert into employees (emp_no, birth_date, first_name, last_name, gender, hire_date) values (999901, '1997-04-15', 'John', 'Smith', 'M', '2022-03-27');
 select * from employees order by emp_no desc;
--- EXERCISE 36
+-- TASK 37
 select * from titles limit 10;
 insert into titles (emp_no, title, from_date) values (999903, 'Senior Engineer', '1997-08-01');
 select * from employees;
 insert into employees values (999903, '1977-09-14', 'Johnathan', 'Creek', 'M', '1999-01-01');
 select * from employees where first_name = 'Johnathan';
--- EXERCISE 27
+-- TASK 38
 select * from dept_emp;
 insert into dept_emp values (999903, 'd005', '1997-08-01', '9999-01-01');
 select * from dept_emp order by emp_no desc;
@@ -246,14 +246,14 @@ create table departments_dub (dept_no char (4) not null, dept_name varchar (40) 
 select * from departments_dub;
 insert into departments_dub select * from departments;
 select * from departments_dub order by dept_no asc;
--- EXERCISE 28 
+-- TASK 39 
 INSERT INTO departments values ('d010', 'Business Analysis');
 select * from departments order by dept_no desc;
 -- UPDATE STATEMENT
 select * from employees where first_name = 'john';
 update employees set first_name = 'samuel', last_name = 'Fadare', birth_date = '1997-04-15' where emp_no = 999901;
 select * from employees where emp_no = 999901;
--- EXERCISE 29
+-- TASK 40
 select * from departments where dept_name = 'Business Analysis';
 insert into departments value ('d011', 'mechanic');
 select * from departments where dept_name like 'bus%';
@@ -272,7 +272,7 @@ rollback;
 select * from departments_dub where dept_no = 'd009';
 insert into departments values ('d009', 'Customer Service');
 select * from departments where dept_no = 'd010';
--- EXERCISE 30
+-- TASK 41
 delete from departments where dept_no = 'd010';
 -- AGGREGATE FUNCTIONS
 select * from salaries;
@@ -376,7 +376,7 @@ FROM
     departments_dub d
         LEFT JOIN
     dept_manager_dub m ON d.dept_no = m.dept_no;
--- EXERCISE 32
+-- TASK 42
 select * from employees;
 select * from dept_manager;
 select e.emp_no, e.first_name, e.last_name, e.gender, e.hire_date, d.dept_no, d.from_date, d.to_date
@@ -410,7 +410,7 @@ join departments f on d.dept_no = f.dept_no
 join salaries s on s.emp_no = d.emp_no
 group by e.first_name
 order by s.salary desc;
--- EXERCISE 33
+-- TASK 43
 select * from employees;
 select * from dept_manager;
 select d.emp_no, d.dept_no, e.first_name, e.last_name, e.hire_date
@@ -418,7 +418,7 @@ from dept_manager d, employees e
 where d.emp_no = e.emp_no;
 -- PREVENT ERROR CODE
 set @@global.sql_mode := replace(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
--- EXERCISE 34
+-- TASK 44
 select * from employees;
 select * from titles;
 select e.first_name, e.last_name, e.hire_date, e.emp_no, t.title
@@ -429,7 +429,7 @@ where e.first_name = 'Margareta' and e.last_name = 'Markovitch';
 select * from employees;
 select * from salaries;
 select e.emp_no, s.salary from employees e cross join salaries s order by salary desc limit 10;
--- EXERCISE 35
+-- TASK 45
 select * from employees;
 select * from dept_emp;
 select * from departments;
@@ -457,7 +457,7 @@ FROM
     departments d ON d.dept_no = o.dept_no
 ORDER BY s.salary DESC;
 select e.emp_no, e.first_name, e.last_name, e.gender, d.dept_name from employees e cross join departments d;
--- EXERCISE 36
+-- TASK 46
 select * from dept_manager;
 select * from departments;
 SELECT 
@@ -467,7 +467,7 @@ FROM
         CROSS JOIN
     departments d
 where d.dept_no = 'd009';
--- EXERCISE 37
+-- TASK 47
 select * from employees;
 select * from departments;
 SELECT 
@@ -504,23 +504,23 @@ FROM
     dept_emp o ON e.emp_no = o.emp_no
         JOIN
     departments d ON o.dept_no = d.dept_no;
--- EXERCISE 38 SALARY MADE BY EACH DEPARTMENTS
+-- TASK 48 SALARY MADE BY EACH DEPARTMENTS
 select * from departments order by dept_no asc;
 select * from salaries; 
 select * from dept_emp;
 select e.dept_no, d.dept_name, sum(s.salary) as 'salary_sum' from departments d join dept_emp e on d.dept_no = e.dept_no join salaries s on s.emp_no = e.emp_no group by e.dept_no order by s.salary asc;
--- EXERCISE 39
+-- TASK 49
 select * from dept_manager; -- f
 select * from departments; -- d
 select * from employees; -- e
 select * from titles; -- t
 select e.first_name, e.last_name, e.hire_date, t.title, t.from_date, t.to_date, d.dept_no from employees e join titles t on t.emp_no = e.emp_no join dept_manager f on f.emp_no = e.emp_no join departments d on d.dept_no = f.dept_no where t.title = 'manager';
--- EXERCISE 30
+-- TASK 50
 select * from salaries;
 select * from dept_manager;
 select * from departments;
 select d.dept_name, avg(s.salary) as 'avg_salary' from departments d join dept_manager f on d.dept_no = f.dept_no join salaries s on s.emp_no = f.emp_no group by d.dept_name; 
--- EXERCISE 31
+-- TASK 51
 select * from employees;
 select * from titles;
 select * from departments;
